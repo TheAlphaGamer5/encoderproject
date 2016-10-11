428d2890e8c7136e75719f1d86d4c4c2705e4ad0
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-10-11 11:30:13 -0700

    UGGGGGGGGGGGGGGGGGGGGGGHHHH

--- encoder.js
+++ encoder.js
@@ -1,14 +1,6 @@
-var inputTalk = document.getElementById("inputArea").value;
 
-function translate(){
-  question = "User: " + inputTalk + "<br>";
-  document.getElementById("textArea").innerHTML += question;
-  console.log("y u no work");
-}
 
-function uniKeyCode(event) {
-  var key = event.keyCode;
-  if (key == 13) {
-    translate();
-  }
-}
+$("#inputText").keyup(function(e){
+  var inputText = document.getElementById("inputArea").value;
+  document.getElementById("textArea").innerHTML = inputText;
+});
