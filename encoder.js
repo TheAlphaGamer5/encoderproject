cb237500a56af20541925e91d851ac861d0c93b4
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-10-12 11:25:07 -0700

    making progress

--- encoder.js
+++ encoder.js
@@ -1,6 +1,21 @@
-
-
-$("#inputText").keyup(function(e){
-  var inputText = document.getElementById("inputArea").value;
+$("#inputArea").keyup(function(e){
+  var inputText = document.getElementById("inputText").value;
   document.getElementById("textArea").innerHTML = inputText;
 });
+
+
+
+$("input[type='radio']").click(function(){
+  $("input:checked").prop('checked',false );
+  $(this).prop('checked',true);
+});
+
+if ($("input:checked").val() == "echo") {
+  console.log("echo");
+}
+if ($("input:checked").val() == "caesarCipher") {
+  console.log("caesarCipher");
+}
+if ($("input:checked").val() == "runes") {
+  console.log("runes");
+}
