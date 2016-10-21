8adecc66c33e34ebe8830694f43f546883f43422
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-10-21 08:13:20 -0700

    so much to do so little time

--- encoder.js
+++ encoder.js
@@ -1,30 +1,62 @@
+/* Normal key code for alphabet
+Normalkeycode = {
+	65:"a",
+	66:"b",
+	67:"c",
+	68:"d",
+	69:"e",
+	70:"f",
+	71:"g",
+	72:"h",
+	73:"i",
+	74:"J",
+  75:"k",
+  76:"L",
+  77:"m",
+  78:"n",
+  79:"o",
+  80:"p",
+  81:"q",
+  82:"r",
+  83:"s",
+  84:"t",
+  85:"u",
+  86:"v",
+  87:"w",
+  88:"x",
+  89:"y",
+  90:"z",
+	32:" "
+}
+	*/
 var caesarCipherEncoder = {
-	"a": "z",
-	"b": "a",
-	"c": "b",
-	"d": "c",
-	"e": "d",
-	"f": "e",
-	"g": "f",
-	"h": "g",
-	"i": "h",
-	"J": "i",
-  "k": "j",
-  "L": "k",
-  "m": "l",
-  "n": "m",
-  "o": "n",
-  "p": "o",
-  "q": "p",
-  "r": "q",
-  "s": "r",
-  "t": "s",
-  "u": "t",
-  "v": "u",
-  "w": "v",
-  "x": "w",
-  "y": "x",
-  "z": "y"
+  65: "z",
+	66: "a",
+	67: "b",
+	68: "c",
+	69: "d",
+	70: "e",
+	71: "f",
+	72: "g",
+	73: "h",
+	74: "i",
+  75: "j",
+  76: "k",
+  77: "l",
+  78: "m",
+  79: "n",
+  80: "o",
+  81: "p",
+  82: "q",
+  83: "r",
+  84: "s",
+  85: "t",
+  86: "u",
+  87: "v",
+  88: "w",
+  89: "x",
+  90: "y",
+	32: " "
 }
 
 
@@ -37,8 +69,7 @@ $("#inputArea").keyup(function(e){
   }
   else if ($("input:checked").val() == "caesarCipher") {
     console.log("caesarCipher");
-    caesarCipherEncoder
-    $("#textArea").append(caesarCipherEncoder[]);
+    document.getElementById("textArea").innerHTML += caesarCipherEncoder[e.keycode];
   }
   else if ($("input:checked").val() == "runes") {
     console.log("runes");
